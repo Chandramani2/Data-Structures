084aa04acc06535e3d7dc94004d3891b10f58f1c
Chandramani2 <chandramanic90@gmail.com>
2025-04-01 00:38:20 +0530

    Rotate Array K Times Brute Force and Optimized

---
+++ Arrays/RotateArrayKTimes.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+////////////////////////////////////////////////
+
+void rotateArrayOnce(int arr[], int size){
+    int tmp = arr[size-1];
+    for(int i=size-1; i>0;i--){
+        arr[i] = arr[i-1];
+    }
+    arr[0] = tmp;
+}
+
+void rotateArrayKTimesBruteForce(int arr[], int size, int k){
+    for(int i=0; i<k; i++){
+     rotateArrayOnce(arr,size);
+    }
+}
+
+////////////////////////////////////////////////
+
+void rotateArray(int arr[], int size, int L, int R){
+    while(L<=R){
+        int tmp = arr[L];
+        arr[L] = arr[R];
+        arr[R] = tmp;
+        L++; R--;
+    }
+}
+
+void rotateArrayOptimized(int arr[], int size, int k){
+    rotateArray(arr,size,size-k,size-1);
+    rotateArray(arr,size,0,size-k-1);
+    rotateArray(arr,size,0,size-1);
+}
+
+////////////////////////////////////////////////
+
+void printArray(int arr[], int size){
+    cout<<"Output Array: ";
+    for(int i=0; i<size; i++){
+        cout<< arr[i] << " ";
+    }
+    cout<<endl;
+}
+
+int main(){
+    int arr[] = {2,1,0,1,-2,3,4,-1,2,-6,2,1};
+    int size = sizeof(arr)/sizeof(arr[0]);
+    int k;
+    cin>>k;
+    printArray(arr,size);
+    rotateArrayKTimesBruteForce(arr,size,k);
+    printArray(arr,size);
+    rotateArrayOptimized(arr,size,k);
+    printArray(arr,size);
+    return 0;
+}
