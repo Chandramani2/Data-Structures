54152ab5613e2b9d7f45eb539a3ea3c5ee0c3abe
Chandramani2 <chandramanic90@gmail.com>
2025-04-05 22:07:02 +0530

    count Primes sieve of eratosthenes

--- Maths/NoOfPrimes.cpp
+++ Maths/NoOfPrimes.cpp
@@ -31,11 +31,34 @@ int countPrimesBruteForce(int N){
     return cnt;
 }
 
+//sieve of eratosthenes
+int countPrimesOptimized(int N){
+    bool p[N+1];
+    fill(p, p+N+1, true);
+    p[0] = p[1] = false;
+    for(int i=2; i<=N; i++){
+        if(p[i]){
+            // we need to move on multiples of i
+            for(int j=2*i;j<=N;j+=i){
+                p[j] = false;
+            }
+        }
+    }
+    // Iterate on P Array and check how many True's
+    int cnt=0;
+    for(int i=0;i<=N;i++){
+        if(p[i]) {cnt++;}
+    }
+    return cnt;
+}
+
 int main(){
     int N;
     cin>>N;
     cout<<isPrimeBruteForce(N)<<endl;
     cout<<isPrimeOptimized(N)<<endl;
     cout<<"No. Of Primes from 1 to "<<N<<" is: "<< countPrimesBruteForce(N)<<endl;
+    cout<<"No. Of Primes from 1 to "<<N<<" is: "<< countPrimesOptimized(N)<<endl;
+
     return 0;
 }
