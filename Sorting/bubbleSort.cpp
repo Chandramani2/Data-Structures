1828832957398e4803c9ef008b68848af8c0b7a9
Chandramani2 <chandramanic90@gmail.com>
2025-12-08 15:06:11 +0530

    Bubble sort Optimized

--- Sorting/bubbleSort.cpp
+++ Sorting/bubbleSort.cpp
@@ -1,17 +1,25 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-void bubbleSort(vector<int> &arr, int n){
-	for(int i = 0 ; i < n-1 ; i++){
-		int cnt = 0;
-		for(int j=0; j < n-i-1; j++){
-			if(arr[j] > arr[j+1]){
-				swap(arr[j], arr[j+1]);
-				cnt++;
-			}
-		}
-		if(cnt==0) break;
-	}
+void bubbleSort(vector<int> &arr) {
+    int n = arr.size();
+    
+    // Outer loop for passes
+    for (int i = 0; i < n - 1; i++) {
+        bool swapped = false; // Use boolean flag
+        
+        // Inner loop for comparison
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+                swapped = true;
+            }
+        }
+        
+        // Optimization: If no two elements were swapped by inner loop, then break
+        if (!swapped)
+            break;
+    }
 }
 
 void printArray(vector<int> &arr){
@@ -21,8 +29,7 @@ void printArray(vector<int> &arr){
 }
 int main(){
 	vector<int> arr = {15,4,10,6,1,8,13,14,17,3,12,10,11,6,5,1};
-	int n = arr.size();
-	bubbleSort(arr, n);
+	bubbleSort(arr);
 	printArray(arr);
 	return 0;
 
