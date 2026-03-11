112e5e233754a3edfa37f2ecd07e4a8dfc44f5d4
Chandramani2 <chandramanic90@gmail.com>
2026-03-11 10:21:19 +0530

    Partition Equal Subset Sum

---
+++ DynamicProgramming/Java/partitionEqualSubsetSum.java
@@ -0,0 +1,72 @@
+/*
+    Partition Equal Subset Sum  - Partition into 2 subsets and check both subsets have equal sum
+    s1 = s2 = totalSum/2
+    if totalSum is odd -> notPossible
+ */
+private static boolean canPartition(int[] nums, int n) {
+    int totalSum = 0;
+    for(int i = 0; i < n; i++){
+        totalSum += nums[i];
+    }
+    if(totalSum%2!=0) return false;
+    int target = totalSum/2;
+
+    System.out.println("Is Subset present: " + isSubsetPresent(nums,n,n-1,target));
+
+    int[][] dp = new int[n+1][target+1];
+
+    for(int i=0;i<=n;i++)
+        for(int j=0;j<=target;j++)
+            dp[i][j] = -1;
+
+    System.out.println("Is SubsetMemo present: " + isSubsetPresentMemo(nums,n,n-1, target, dp));
+
+   return isSubsetPresent(nums,n,n-1,target) && isSubsetPresentMemo(nums,n,n-1, target, dp);
+
+}
+
+private static boolean isSubsetPresent(int[] nums, int n, int ind, int target) {
+    if(target == 0) return true;
+    if(ind==0) return (nums[0] == target);
+
+    boolean notTake = isSubsetPresent(nums, n, ind-1, target);
+    boolean take = false;
+    if(target>=nums[ind])
+            take = isSubsetPresent(nums, n, ind-1, target-nums[ind]);
+
+    return notTake || take;
+}
+
+/*
+-1 → uncomputed
+0 → false
+1 → true
+ */
+private static boolean isSubsetPresentMemo(int[] nums, int n, int ind, int target, int[][] dp) {
+    if(target == 0) return true;
+    if(ind==0) return (nums[0] == target);
+
+    if(dp[ind][target]!=-1) return dp[ind][target]==1;
+
+    boolean notTake = isSubsetPresentMemo(nums, n, ind-1, target, dp);
+    boolean take = false;
+    if(target>=nums[ind])
+        take = isSubsetPresentMemo(nums, n, ind-1, target-nums[ind], dp);
+
+    boolean res = take || notTake;
+    dp[ind][target] = res ? 1 : 0;
+    return res;
+}
+
+public static void main(String[] args){
+    int[] nums = {1,2,3,4};
+    int n = nums.length;
+
+    System.out.println("CanPartition: " + canPartition(nums,n));
+}
+
+
+
+
+
+
